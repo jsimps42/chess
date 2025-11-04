9fc5105903c15d83d23fcf9dc27e4f19a9178707
jsimps42 <jsimps1130@gmail.com>
2025-11-04 00:24:38 -0700

    Recovered some stuff to use as a baseline...

---
+++ server/src/main/java/dataaccess/MySQLGameAccess.java
@@ -0,0 +1,121 @@
+package dataaccess;
+
+import model.GameData;
+import java.sql.*;
+import java.util.HashSet;
+
+import com.google.gson.Gson;
+
+import chess.ChessGame;
+
+import static dataaccess.MySQLHelper.executeUpdate;
+import static dataaccess.MySQLHelper.configureDatabase;
+
+public class MySQLGameAccess implements GameAccess{
+    private final String[] createGameStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+                game TEXT NOT NULL,
+                gameID int NOT NULL,
+                gameName VARCHAR(255) NOT NULL,
+                whiteUsername VARCHAR(255),
+                whiteUsername VARCHAR(255)               
+                PRIMARY KEY (gameID),
+                INDEX(gameName),
+                INDEX(whiteUsername)
+                INDEX(blackUsername)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    public MySQLGameAccess() throws DataAccessException {
+        configureDatabase(createGameStatement);
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        try {
+            String statement = "INSERT INTO game (chessGame, gameId, gameName, whiteUsername, blackUsername) VALUES (?, ?, ?, ? ,?)";
+            String chessGameJson = new Gson().toJson(game);
+            executeUpdate(statement, chessGameJson, game.gameID(), game.gameName(), game.whiteUsername(), game.blackUsername());
+        } catch (DataAccessException e) {
+            throw new DataAccessException(String.format("Error creating game: %s", e.getMessage()));
+        }    
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT chessGame, gameId, gameName, whiteUsername, blackUsername FROM game WHERE gameId=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new GameData(
+                          new Gson().fromJson(rs.getString("chessGame"), ChessGame.class), 
+                          rs.getInt("gameId"), 
+                          rs.getString("gameName"), 
+                          rs.getString("whiteUsername"), 
+                          rs.getString("blackUsername")
+                        );
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Error retrieving game: %s", e));
+        }
+        return null;
+    }
+
+    @Override
+    public boolean gameExists(int gameID) {
+        try {
+            getGame(gameID);
+        } catch (DataAccessException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        executeUpdate(
+          "UPDATE game SET chessGame=?, gameName=?, whiteUsername=?, blackUsername=? WHERE gameId=?", 
+          new Gson().toJson(game),
+          game.gameName(),  
+          game.whiteUsername(), 
+          game.blackUsername(), 
+          game.gameID()
+        );
+    }
+
+    @Override
+    public HashSet<GameData> listGames() throws DataAccessException {
+        HashSet<GameData> result = new HashSet<>(16);
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT chessGame, gameId, gameName, whiteUsername, blackUsername FROM game";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        result.add(new GameData(
+                          new Gson().fromJson(rs.getString("chessGame"), ChessGame.class), 
+                          rs.getInt("gameId"), 
+                          rs.getString("gameName"), 
+                          rs.getString("whiteUsername"), 
+                          rs.getString("blackUsername")
+                          ));
+                    }
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Error retrieving games: %s", e.getMessage()));
+        }    
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "TRUNCATE game";
+        executeUpdate(statement);
+    }
+}
