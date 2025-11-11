0121482f6f9c27e752438effc2465583b1fc6721
jsimps42 <jsimps1130@gmail.com>
2025-11-11 13:45:01 -0700

    Updated imports in serverFacade and created the ResponseException

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,5 +1,15 @@
 package server;
 
+import com.google.gson.Gson;
+import model.*;
+import exception.ResponseException;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class ServerFacade {
     
 }
