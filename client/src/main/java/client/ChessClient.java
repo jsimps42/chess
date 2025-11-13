36fb9d03edec644599322d45ebe903dfb2fef986
jsimps42 <jsimps1130@gmail.com>
2025-11-12 23:08:19 -0700

    Beginning of ChessClient

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,5 +1,26 @@
 package client;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import model.*;
+import exception.ResponseException;
+import server.ServerFacade;
+
+import static ui.EscapeSequences.*;
+
 public class ChessClient {
     
+    private String username = null;
+    private final ServerFacade server;
+    private State state = State.SIGNEDOUT;
+
+    public ChessClient(String serverUrl) throws Exception {
+        server = new ServerFacade(serverUrl);
+    }
+    
+    public enum State {
+        SIGNEDOUT,
+        SIGNEDIN
+    }
 }
